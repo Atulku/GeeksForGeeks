1ea7ebed73e139dcf89e42736e9250c9058b32d9
Atul <kumar61atul@gmail.com>
2020-10-18 13:00:37 +0530

    Test

--- src/MoveAllNegaiveNumber.java
+++ src/MoveAllNegaiveNumber.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class MoveAllNegaiveNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array length ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the array: ");
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
 	}
 
 }
